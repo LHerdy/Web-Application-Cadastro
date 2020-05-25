d7a057eb6aefad4a2877c6d4fd7730b193dc6e4a
Leandro Herdy <le.herdy@gmail.com>
2020-05-25 11:58:45 -0300

    Primeiro commit sistema configurado.

---
+++ src/main/java/br/sistema/cadastro/controller/MainController.java
@@ -0,0 +1,17 @@
+package br.sistema.cadastro.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+	@RequestMapping("/")
+	public String login(Model model, HttpServletRequest request) {
+		String mensagem = request.getParameter("mensagem");
+		model.addAttribute("mensagem", mensagem);
+		return "login";
+	}
+}
